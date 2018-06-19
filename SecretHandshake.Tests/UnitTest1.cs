20cb9c7649dae027a6961d1285dfd84b40f651de
jeswin <jeswinpk@agilehead.com>
2018-06-19 18:48:31 +0530

    Initial commit. Setting up.

---
+++ SecretHandshake.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SecretHandshake.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ SecretHandshake/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SecretHandshake
+{
+    public class Class1
+    {
+    }
+}
